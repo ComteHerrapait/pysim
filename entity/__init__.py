66ac85f53ee53eb8e1933c58afbde103edf44aeb
ComteHerrapait <leon.delcroix@telecom-st-etienne.fr>
2020-06-07 18:47:30 +0200

    implementation

--- entity/__init__.py
+++ entity/__init__.py
@@ -5,9 +5,10 @@ Created on Tue Jun  2 20:31:52 2020
 @author: ComteHerappait
 """
 import pygame
-from math import atan2, pi, sin, cos, sqrt
+from math import atan2, pi, sqrt
 from random import uniform, gauss
 import numpy as np
+from myVector import myVector
 
 MAX_SPEED = 3
 FRIEND_RADIUS = 120
@@ -18,7 +19,7 @@ FACTOR_ALIGN   = 1
 FACTOR_REJECT  = 1
 FACTOR_ATTRACT = 1
 FACTOR_NOISE   = 1
-FACTOR_DEBUG = True
+FACTOR_DEBUG = False
 
 def addVectors(v1,v2):
     return [v1[0]+v2[0],v1[1]+v2[1]]
@@ -30,7 +31,7 @@ class Entity:
         """constructor"""
         #print("entity : ",screensize, speed, screen)
         self.screen = screen
-        self.speed_ = speed
+        self.speed_ = myVector(speed[0],speed[1])
         self.width_, self.height_ = screen.get_size()
         img = pygame.image.load("arrow-white.png")
         self.image_source_ = pygame.transform.scale(img, (INDIVIDUAL_SIZE,INDIVIDUAL_SIZE))
@@ -43,11 +44,17 @@ class Entity:
             
         self.getFriends(others)
                 
-        self.groupWithFriends()
-        self.alignOnFriends()
-        self.dodgeFriends()
-        self.addNoise(0.05)
+        _group = self.groupWithFriends()
+        _align = self.alignOnFriends()
+        _dodge = self.dodgeFriends()
+        #_noise = self.addNoise()
         
+        self.speed_.add(_group)
+        self.speed_.add(_align)
+        self.speed_.add(_dodge)
+        #self.speed_.add(_noise)
+        
+        #self.speed_.rotate(gauss(0,1)/50)
         self.limitSpeed()
         
         self.move()
@@ -58,20 +65,11 @@ class Entity:
             pygame.draw.line(self.screen, (0,255,0), self.getPos(), f.getPos(), 1)
                     
     def move(self):
-        self.position_.move_ip(self.speed_)
-        angle = -45 + atan2(self.speed_[0],self.speed_[1])*180/pi
+        self.position_.move_ip(self.speed_.array())
+        
             #returns final state of sprite
-        self.image_ = pygame.transform.rotate(self.image_source_, angle)
+        self.image_ = pygame.transform.rotate(self.image_source_, self.speed_.getAngle()+45) #angle offset because of the image used
         
-    def turn(self, alpha):
-        alpha = alpha*180/pi /10000
-        x = self.speed_[0]
-        y = self.speed_[1]
-        self.speed_[0] = cos(alpha)*x - sin(alpha)*y
-        self.speed_[1] = sin(alpha)*x + cos(alpha)*y
-        
-        angle = -45 + atan2(self.speed_[0],self.speed_[1])*180/pi
-        self.image_ = pygame.transform.rotate(self.image_source_, angle)
             
     def warpOnEdges(self):
         BORDER_X = 100
@@ -87,83 +85,63 @@ class Entity:
             
         if (self.position_[1] > self.height_ - BORDER_X) :
             self.position_[1] = BORDER_Y
-            
-        #self.position_[0] = (self.position_[0] + self.width_) % self.width_;
-        #self.position_[1] = (self.position_[1] + self.height_) % self.height_;
-            
+                        
     def distance(self, point):
         return sqrt ( (point[0] - self.position_[0])**2 +
                     (  point[1] - self.position_[1])**2  )
     
     def limitSpeed(self, ceiling = MAX_SPEED):
-        speedNorm = np.linalg.norm(self.speed_)
+        speedNorm = self.speed_.getNorm()
         if speedNorm > ceiling :
-            self.speed_[0] = self.speed_[0] / (speedNorm/ceiling)
-            self.speed_[1] = self.speed_[1] / (speedNorm/ceiling)
+            self.speed_.multiply(ceiling/self.speed_.getNorm())
                     
     def getFriends(self, others):
         neighbors = [e for e in others if ( 0 < self.distance(e.position_) and self.distance(e.position_) < FRIEND_RADIUS )]
         self.friends = neighbors
     
     def alignOnFriends(self):
-        if len(self.friends) == 0 : return
-        totalSpeed = (0,0)
+        if len(self.friends) == 0 : return myVector(0,0)
+        totalSpeed = myVector(0,0)
         for f in self.friends:
-            totalSpeed = addVectors(totalSpeed,f.speed_)
-        averageSpeed = [FACTOR_ALIGN * totalSpeed[0]/len(self.friends),
-                        FACTOR_ALIGN * totalSpeed[1]/len(self.friends)]
-        self.speed_ = addVectors(self.speed_,averageSpeed)
-        if FACTOR_DEBUG : print("align:\t", round(np.linalg.norm(averageSpeed),2), "|",averageSpeed)
+            totalSpeed.add(f.speed_)
+        totalSpeed.normalize()
+        return totalSpeed
         
-    def addNoise(self, standardDeviation):
-        
-        noise = [FACTOR_NOISE * gauss(0,standardDeviation) ,
-                 FACTOR_NOISE * gauss(0,standardDeviation) ]
-        if (uniform(0,10) < 7) : noise = [0, 0]
-        self.speed_ = addVectors(self.speed_,noise)
-        if FACTOR_DEBUG : print("noise:\t", round(np.linalg.norm(noise),2), "|",noise)
+    def addNoise(self):
+        if (uniform(0,10) > 7) :
+            noise = myVector( gauss(0,1) ,
+                              gauss(0,1) )
+            noise.normalize()
+            return noise
+        else:
+            return myVector(0,0)
     
     def groupWithFriends(self):
-        if len(self.friends) == 0 : return
+        if len(self.friends) == 0 : return myVector(0,0)
         friends_pos = [f.getPos() for f in self.friends]
         group_pos = np.average(friends_pos, axis=0)
-        vector = [FACTOR_ATTRACT * (group_pos[0] - self.position_[0]),
-                  FACTOR_ATTRACT * (group_pos[1] - self.position_[1])]
-        self.speed_ = addVectors(self.speed_,vector)
-        if FACTOR_DEBUG : print("group:\t", round(np.linalg.norm(vector),2), "|",vector)
+        vector = myVector( (group_pos[0] - self.position_[0]) ,
+                           (group_pos[1] - self.position_[1]) )
+        vector.normalize()
+        return vector
         
     def dodgeFriends(self):
-        if len(self.friends) == 0 : return
-        total = [0,0]
+        if len(self.friends) == 0 : return myVector(0,0)
+        total = myVector(0,0)
         for f in self.friends:
             if (self.distance(f.getPos()) < CONFORT_ZONE):
                 pos = f.getPos()
                 d = self.distance(pos)
-                vect = [self.position_[0]-pos[0], self.position_[1]-pos[1]]
-                weighted = [FACTOR_REJECT * vect[0]/d,
-                            FACTOR_REJECT * vect[1]/d]
-                total = addVectors(total, weighted)
-        self.speed_ = addVectors(self.speed_,total)
-        if FACTOR_DEBUG : print("dodge:\t", round(np.linalg.norm(total),2), "|",total)        
+                vect = myVector ( self.position_[0]-pos[0], self.position_[1]-pos[1] )
+                vect.multiply(1/d)
+                total.add(vect)
+        total.normalize()
+        return total  
         #pygame.draw.circle(self.screen, (255,0,0), self.getPos(), CONFORT_ZONE, 1)
     
     def getPos(self):
         return [ self.position_[0], self.position_[1]]
-    
-    def reboundOnEdges(self, hardborders=False):
-        #change direction when hitting wall
-        if self.position_.left < 0 or self.position_.right > self.width_:
-            self.speed_[0] = -self.speed_[0]
-        if self.position_.top < 0 or self.position_.bottom > self.height_:
-            self.speed_[1] = -self.speed_[1]
-            
-        #prevents objects from exiting the screen
-        if hardborders:
-            self.position_.left  = max(self.position_.left ,0)
-            self.position_.bottom  = max(self.position_.bottom ,0)
-            self.position_.right = min(self.position_.right,self.width_)
-            self.position_.top = min(self.position_.top,self.height_)
-            
+                
     def __del__(self):
         """destructor"""
         pass
--- myVector/__init__.py
+++ myVector/__init__.py
@@ -1,5 +1,6 @@
 #
 from numpy.linalg import norm
+from math import atan2, pi, sin, cos
 
 class myVector:
     
@@ -19,4 +20,24 @@ class myVector:
         self.y *= value
         
     def normalize(self):
-        self.multiply(1/self.getNorm())
+        if self.getNorm() == 0 : 
+            return myVector(0,0)
+        else :
+            self.multiply(1/self.getNorm())
+        
+    def getAngle(self):
+        return - atan2(self.y,self.x)*180/pi
+    
+    def rotate(self, angle):
+        angle = - angle * pi / 180
+        old_x = self.x
+        old_y = self.y
+        self.x = cos(angle)*old_x - sin(angle)*old_y
+        self.y = sin(angle)*old_x + cos(angle)*old_y
+        
+    def array(self):
+        return [self.x, self.y]
+    
+    def display(self) :
+        print("x:",self.x,"\ty:",self.y,
+              "\nangle:",self.getAngle(),"\tnorm :", self.getNorm() )
